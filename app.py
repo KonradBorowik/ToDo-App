24c6414b83ca8afc2254bd48b95625ee3f2a8a9a
KonradBorowik <konrad1borowik@gmail.com>
2024-05-19 23:01:08 +0200

    different approach

--- app.py
+++ app.py
@@ -68,9 +68,5 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        app.run(debug=True)
-    
+if __name__ == "__main__":    
     app.run(debug=True)
